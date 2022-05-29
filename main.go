4eb6c72424cd8f1e24367a0a78dfb1af4c070ee2
WhiteDiamondz <fab.debatisse@hotmail.es>
2022-05-29 20:53:33 +0200

    Start to transition

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"net/http"
 
-	"github.com/Brawdunoir/dionysos-server/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
-	router.POST("/users", routes.UserPost)
+	// router.POST("/users", routes.UserCreate)
 	router.GET("/ping", getping)
 
 	router.Run(":8080")
