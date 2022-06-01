6280083e23fb7350b4870b460cbda2e57749fa32
Fabien D <49648494+WhiteDiamondz@users.noreply.github.com>
2022-06-01 20:32:53 +0200

    3 switch setup neo4j (#13)
    * Transition from from MongoDB to Neo4j

--- main.go
+++ main.go
@@ -2,20 +2,45 @@ package main
 
 import (
 	"net/http"
+	"os"
 
-	"github.com/Brawdunoir/dionysos-server/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
 )
 
 func main() {
+	_, err := getNeoDriver()
+	if err != nil {
+		panic(err)
+	}
+
 	router := gin.Default()
 
-	router.POST("/users", routes.UserPost)
 	router.GET("/ping", getping)
-
-	router.Run(":8080")
+	err = router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func getping(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"response": "pong"})
+}
+
+func getNeoDriver() (neo4j.Driver, error) {
+	neo4jURI, found := os.LookupEnv("NEO4J_URI")
+	if !found {
+		panic("NEO4J_URI not set")
+	}
+	neo4jUsername, found := os.LookupEnv("NEO4J_USERNAME")
+	if !found {
+		panic("NEO4J_USERNAME not set")
+	}
+	neo4jPassword, found := os.LookupEnv("NEO4J_PASSWORD")
+	if !found {
+		panic("NEO4J_PASSWORD not set")
+	}
+
+	driver, err := neo4j.NewDriver(neo4jURI, neo4j.BasicAuth(neo4jUsername, neo4jPassword, ""))
+	return driver, err
 }
