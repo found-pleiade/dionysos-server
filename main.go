4a69717eab1d0ed51470ccbee483a987905d3dfd
WhiteDiamondz <fab.debatisse@hotmail.es>
2022-06-01 18:24:05 +0200

    Cleaning and focusing on Neo4j changes

--- main.go
+++ main.go
@@ -2,51 +2,17 @@ package main
 
 import (
 	"net/http"
-	"os"
 
-	"github.com/Brawdunoir/dionysos-server/pkg/users"
-	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	neo4jUri, found := os.LookupEnv("NEO4J_URI")
-	if !found {
-		panic("NEO4J_URI not set")
-	}
-	neo4jUsername, found := os.LookupEnv("NEO4J_USERNAME")
-	if !found {
-		panic("NEO4J_USERNAME not set")
-	}
-	neo4jPassword, found := os.LookupEnv("NEO4J_PASSWORD")
-	if !found {
-		panic("NEO4J_PASSWORD not set")
-	}
+	router := gin.Default()
 
-	usersRepository := users.UserNeo4jRepository{
-		Driver: driver(neo4jUri, neo4j.BasicAuth(neo4jUsername, neo4jPassword, "")),
-	}
-	registrationHandler := &users.UserRegistrationHandler{
-		Path:           "/users",
-		UserRepository: &usersRepository,
-	}
-	loginHandler := &users.UserLoginHandler{
-		Path:           "/users/login",
-		UserRepository: &usersRepository,
-	}
-
-	server := http.NewServeMux()
-	server.HandleFunc(registrationHandler.Path, registrationHandler.Register)
-	server.HandleFunc(loginHandler.Path, loginHandler.Login)
-
-	if err := http.ListenAndServe(":3000", server); err != nil {
-		panic(err)
-	}
+	router.GET("/ping", getping)
+	router.Run(":8080")
 }
 
-func driver(target string, token neo4j.AuthToken) neo4j.Driver {
-	result, err := neo4j.NewDriver(target, token)
-	if err != nil {
-		panic(err)
-	}
-	return result
+func getping(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"response": "pong"})
 }
