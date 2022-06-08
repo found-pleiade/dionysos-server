911e5eb0c84d9942b217a7aa5940a31d652769d2
WhiteDiamondz <fab.debatisse@hotmail.es>
2022-06-08 22:29:53 +0200

    Change context from background to 500ms timeout

--- routes/room.go
+++ routes/room.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/Brawdunoir/dionysos-server/database"
 	"github.com/Brawdunoir/dionysos-server/models"
@@ -13,18 +14,20 @@ import (
 
 // CreateRoom creates a room in the aganro database
 func CreateRoom(c *gin.Context) {
+	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	defer cancelCtx()
 	var room models.Room
 	if err := c.ShouldBindJSON(&room); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	col, err := db.Collection(context.Background(), database.RoomsCollection)
+	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	meta, err := col.CreateDocument(context.Background(), room)
+	meta, err := col.CreateDocument(ctx, room)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not created"})
@@ -36,15 +39,17 @@ func CreateRoom(c *gin.Context) {
 
 // GetRoom returns a room from the aganro database
 func GetRoom(c *gin.Context) {
+	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	defer cancelCtx()
 	var result models.Room
 	id := c.Param("id")
 
-	col, err := db.Collection(context.Background(), database.RoomsCollection)
+	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	_, err = col.ReadDocument(context.Background(), id, &result)
+	_, err = col.ReadDocument(ctx, id, &result)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not found"})
@@ -56,6 +61,8 @@ func GetRoom(c *gin.Context) {
 
 // UpdateRoom updates a room in the aganro database
 func UpdateRoom(c *gin.Context) {
+	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	defer cancelCtx()
 	var room models.Room
 	id := c.Param("id")
 
@@ -64,7 +71,7 @@ func UpdateRoom(c *gin.Context) {
 		return
 	}
 
-	col, err := db.Collection(context.Background(), database.RoomsCollection)
+	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -73,7 +80,7 @@ func UpdateRoom(c *gin.Context) {
 		"name": room.Name,
 	}
 
-	meta, err := col.UpdateDocument(context.Background(), id, patch)
+	meta, err := col.UpdateDocument(ctx, id, patch)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not modified"})
@@ -85,14 +92,16 @@ func UpdateRoom(c *gin.Context) {
 
 // DeleteRoom deletes a room in the aganro database
 func DeleteRoom(c *gin.Context) {
+	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	defer cancelCtx()
 	id := c.Param("id")
 
-	col, err := db.Collection(context.Background(), database.RoomsCollection)
+	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	meta, err := col.RemoveDocument(context.Background(), id)
+	meta, err := col.RemoveDocument(ctx, id)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not deleted"})
