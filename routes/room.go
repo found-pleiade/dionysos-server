a215bd001223358fddf2cd74e18d7f520e5a4151
WhiteDiamondz <fab.debatisse@hotmail.es>
2022-06-08 23:11:51 +0200

    Add log on error for future debugging <3

--- routes/room.go
+++ routes/room.go
@@ -3,7 +3,7 @@ package routes
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -14,23 +14,24 @@ import (
 
 // CreateRoom creates a room in the aganro database
 func CreateRoom(c *gin.Context) {
-	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	ctx, cancelCtx := context.WithTimeout(c, 1000*time.Millisecond)
 	defer cancelCtx()
 	var room models.Room
 	if err := c.ShouldBindJSON(&room); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Printf("Failed to bind JSON: %v", err)
 		return
 	}
 
 	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
-		fmt.Println(err)
+		log.Printf("Failed to access collection: %v", err)
 	}
 
 	meta, err := col.CreateDocument(ctx, room)
-
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not created"})
+		log.Printf("Failed to create document: %v", err)
 		return
 	}
 
@@ -39,20 +40,20 @@ func CreateRoom(c *gin.Context) {
 
 // GetRoom returns a room from the aganro database
 func GetRoom(c *gin.Context) {
-	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	ctx, cancelCtx := context.WithTimeout(c, 1000*time.Millisecond)
 	defer cancelCtx()
 	var result models.Room
 	id := c.Param("id")
 
 	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
-		fmt.Println(err)
+		log.Printf("Failed to access collection: %v", err)
 	}
 
 	_, err = col.ReadDocument(ctx, id, &result)
-
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not found"})
+		log.Printf("Failed to get document: %v", err)
 		return
 	}
 
@@ -61,19 +62,20 @@ func GetRoom(c *gin.Context) {
 
 // UpdateRoom updates a room in the aganro database
 func UpdateRoom(c *gin.Context) {
-	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	ctx, cancelCtx := context.WithTimeout(c, 1000*time.Millisecond)
 	defer cancelCtx()
 	var room models.Room
 	id := c.Param("id")
 
 	if err := c.ShouldBindJSON(&room); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		log.Printf("Failed to bind JSON: %v", err)
 		return
 	}
 
 	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
-		fmt.Println(err)
+		log.Printf("Failed to access collection: %v", err)
 	}
 
 	patch := map[string]interface{}{
@@ -81,9 +83,9 @@ func UpdateRoom(c *gin.Context) {
 	}
 
 	meta, err := col.UpdateDocument(ctx, id, patch)
-
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not modified"})
+		log.Printf("Failed to update document: %v", err)
 		return
 	}
 
@@ -92,19 +94,19 @@ func UpdateRoom(c *gin.Context) {
 
 // DeleteRoom deletes a room in the aganro database
 func DeleteRoom(c *gin.Context) {
-	ctx, cancelCtx := context.WithTimeout(c, 500*time.Millisecond)
+	ctx, cancelCtx := context.WithTimeout(c, 1000*time.Millisecond)
 	defer cancelCtx()
 	id := c.Param("id")
 
 	col, err := db.Collection(ctx, database.RoomsCollection)
 	if err != nil {
-		fmt.Println(err)
+		log.Printf("Failed to access collection: %v", err)
 	}
 
 	meta, err := col.RemoveDocument(ctx, id)
-
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": "Room not deleted"})
+		log.Printf("Failed to delete document: %v", err)
 		return
 	}
 
