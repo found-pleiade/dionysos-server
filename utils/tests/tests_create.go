be4d2ec70e3f382d01e5682893be89406b0ef2a9
Yann Lacroix <yann.lacroix.dev@gmail.com>
2022-07-03 00:06:52 +0200

    Add Get and Create tests for user

---
+++ utils/tests/tests_create.go
@@ -0,0 +1,29 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+// TestCreate struct to factorize code inside a single feature test.
+type TestCreate struct {
+	SubTests []SubTest
+}
+
+// Runs a series of tests for a Create type endpoint.
+func (test TestCreate) Run(t *testing.T) {
+	disableLogs()
+
+	for _, subtest := range test.SubTests {
+		t.Run(subtest.Name, func(t *testing.T) {
+			w, err := executeRequest(subtest.Request.Method, subtest.Request.Url, subtest.Request.Body)
+			if err != nil {
+				t.Error(err)
+			}
+
+			assert.Equal(t, w.Code, subtest.ResponseCode)
+			assert.MatchRegex(t, w.Body.String(), subtest.ResponseBodyRegex)
+		})
+	}
+}
