bed90274102942dbb99d4c06011b9a82fcdfe8b7
Yann L <56452324+Brawdunoir@users.noreply.github.com>
2022-07-21 23:46:40 +0200

    56 add go report badge to readme (#63)
    * Correct misspelled word (goreport)
    
    * Add badge

--- routes/db.go
+++ routes/db.go
@@ -5,5 +5,5 @@ import (
 	"gorm.io/gorm"
 )
 
-// db is the database used by the api accross all routes.
+// db is the database used by the api across all routes.
 var db *gorm.DB = database.GetDatabase() //nolint:deadcode,unused,varcheck
