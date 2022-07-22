3048b445ca206601328480887644d33bf80470c9
Yann L <56452324+Brawdunoir@users.noreply.github.com>
2022-07-22 12:04:34 +0200

    Delete base path in POST (create) response (#64)

--- utils/routes/responses.go
+++ utils/routes/responses.go
@@ -1,13 +1,11 @@
 package utils
 
-import "github.com/Brawdunoir/dionysos-server/variables"
-
 type URIResponse struct {
 	URI string `json:"uri"`
 }
 
 func CreateURIResponse(uri string) *URIResponse {
 	return &URIResponse{
-		URI: variables.BasePath + uri,
+		URI: uri,
 	}
 }
