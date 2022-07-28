408cd6f2a2682d6607370b0a5684aba402a7203d
Yann Lacroix <yann.lacroix.dev@gmail.com>
2022-07-28 23:39:24 +0200

    Add basicAuth middleware to handle authenticate and authorization It is applied on all routes except on POST /users because that’s how we create the user in the first place. We also pass the id in the context so other part of the app can know which user calls the API

--- routes/router.go
+++ routes/router.go
@@ -2,9 +2,15 @@
 package routes
 
 import (
+	"context"
+	"crypto/sha256"
+	"crypto/subtle"
+	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/Brawdunoir/dionysos-server/models"
 	"github.com/Brawdunoir/dionysos-server/variables"
 	"github.com/gin-gonic/gin"
 )
@@ -17,15 +23,17 @@ func SetupRouter(router *gin.Engine) *gin.Engine {
 
 	r := router.Group(basePath)
 	{
-		userRouter := r.Group("/users")
+		// We should not use the basicAuth middleware for the /users endpoint because it is used to create the user.
+		r.POST("/users", CreateUser)
+
+		userRouter := r.Group("/users", basicAuth)
 		{
-			userRouter.POST("", CreateUser)
 			userRouter.GET("/:id", GetUser)
 			userRouter.PATCH("/:id", UpdateUser)
 			userRouter.DELETE("/:id", DeleteUser)
 		}
 
-		roomRouter := r.Group("/rooms")
+		roomRouter := r.Group("/rooms", basicAuth)
 		{
 			roomRouter.POST("", CreateRoom)
 			roomRouter.GET("/:id", GetRoom)
@@ -41,6 +49,44 @@ func SetupRouter(router *gin.Engine) *gin.Engine {
 		})
 	}
 	return router
+}
+
+// Middleware to authenticate users.
+func basicAuth(c *gin.Context) {
+	ctx, cancelCtx := context.WithTimeout(c, 1000*time.Millisecond)
+	defer cancelCtx()
+	// Extract the id and password from the request Authorization header.
+	id, password, ok := c.Request.BasicAuth()
+	if ok {
+		// Get user from database.
+		var user models.User
+		err := db.WithContext(ctx).First(&user, id).Error
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+			log.Printf("Failed to find document: %v", err)
+			return
+		}
+		// Calculate SHA-256 hashes for the provided and expected passwords.
+		passwordHash := sha256.Sum256([]byte(password))
+		expectedPasswordHash := sha256.Sum256([]byte(user.Password))
+
+		// Use the subtle.ConstantTimeCompare() function to avoid leaking information.
+		passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+		if passwordMatch {
+			c.Set("id", id)
+			c.Next()
+			return
+		}
+	}
+
+	// If the Authentication header is not present, is invalid, or the
+	// password is wrong, then set a WWW-Authenticate
+	// header to inform the client that we expect them to use basic
+	// authentication and send a 401 Unauthorized response.
+	c.Header("WWW-Authenticate", `Basic id:password charset="UTF-8"`)
+	c.AbortWithStatus(http.StatusUnauthorized)
 }
 
 // Middleware for CORS requests.
