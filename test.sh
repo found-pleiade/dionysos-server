e82c2dcb7c217709397feab7a663f03c8fd046cb
Yann Lacroix <yann.lacroix.dev@gmail.com>
2022-08-19 23:30:21 +0200

    ci: Use codecov instead

--- test.sh
+++ test.sh
@@ -7,8 +7,7 @@ docker-compose -f docker-compose_testing.yaml up --build -d >/dev/null 2>&1
 
 # Load env variables and run tests on the given package
 ENVIRONMENT=TEST GIN_MODE=release POSTGRES_HOST=localhost POSTGRES_USER=$POSTGRES_USER POSTGRES_PASSWORD=$POSTGRES_PASSWORD  POSTGRES_PORT=$POSTGRES_PORT POSTGRES_DB=$POSTGRES_DB \
-go test -failfast -cover -covermode=count -coverprofile=coverage.out -v github.com/Brawdunoir/dionysos-server/$1
-go tool cover -func=coverage.out -o=coverage.out
+go test -failfast -cover -coverprofile=coverage.txt -covermode=atomic -v github.com/Brawdunoir/dionysos-server/$1
 
 # Stop the DB
 docker-compose -f docker-compose_testing.yaml down >/dev/null 2>&1
