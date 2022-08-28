e0c30f0aecbe0cb64e09a48f17a175266ce8f897
Yann Lacroix <yann.lacroix.dev@gmail.com>
2022-08-28 20:19:24 +0200

    fix(Routes): Rename roomid to id in route parameter
    Otherwise Gin will fail booting

--- routes/router.go
+++ routes/router.go
@@ -67,7 +67,7 @@ func SetupRouter(router *gin.Engine) *gin.Engine {
 				roomRouter.PATCH("/:id", invalidateCacheURI, UpdateRoom)
 				roomRouter.PATCH("/:id/connect", invalidateCacheURI, ConnectUserToRoom)
 				roomRouter.PATCH("/:id/disconnect", invalidateCacheURI, DisconnectUserFromRoom)
-				roomRouter.PATCH("/:roomid/kick/:userid", invalidateCacheURI, KickUserFromRoom)
+				roomRouter.PATCH("/:id/kick/:userid", invalidateCacheURI, KickUserFromRoom)
 			} else {
 				roomRouter.GET("/:id", GetRoom)
 				roomRouter.PATCH("/:id", UpdateRoom)
