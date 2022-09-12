bd914d875d3f449a7b1d9eea0856b4cf59172326
Yann Lacroix <yann.lacroix.dev@gmail.com>
2022-09-13 00:33:13 +0200

    feat(middleware): Change error middleware
    Now we have good middleware but zap error log prints a false trace of
    the error because we are calling it from here.

--- middlewares/errors.go
+++ middlewares/errors.go
@@ -1,6 +1,9 @@
 package middlewares
 
 import (
+	"strings"
+
+	utils "github.com/Brawdunoir/dionysos-server/utils/routes"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
@@ -8,13 +11,15 @@ import (
 func ErrorHandler(logger *zap.SugaredLogger) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Next()
+		if len(c.Errors) > 0 {
 
-		for _, ginErr := range c.Errors {
-			logger.Error(ginErr.Error(), ginErr.Meta)
+			for _, ginErr := range c.Errors {
+				logger.Error(ginErr.Error(), ginErr.Meta)
+			}
+
+			// status -1 doesn't overwrite existing status code
+			lastErr := c.Errors[len(c.Errors)-1].Error()
+			c.JSON(-1, utils.CreateErrorResponse(strings.ToUpper(lastErr[0:1])+lastErr[1:]))
 		}
-
-		// status -1 doesn't overwrite existing status code
-		c.JSON(-1, gin.H{"error": "Internal server error"})
-
 	}
 }
